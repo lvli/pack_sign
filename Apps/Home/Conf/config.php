b1850ec7146473c064a58f1def3f699e87a29085
吕力 <2302216679@qq.com>
2016-05-13 10:04:44 +0800

    初始化

---
+++ Apps/Home/Conf/config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	'DB_TYPE'  => 'mysql',
+	'DB_HOST'  => '127.0.0.1',
+	'DB_NAME'  => 'pack_sign',
+	'DB_USER'  => 'root',
+	'DB_PWD'  => '',
+	'DB_PREFIX'  => 'ps_',
+	'DB_CHARSET' => 'utf8',
+	'DB_PORT'  => '3306',
+	'APP_DEBUG'  =>  false,
+);
