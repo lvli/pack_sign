e3bea1fe89eb382354b6c5258d846d263cbdc05d
吕力 <2302216679@qq.com>
2016-05-17 15:22:22 +0800

    修改cronVirus

--- Apps/Home/Conf/config.php
+++ Apps/Home/Conf/config.php
@@ -10,4 +10,13 @@ return array(
 	'DB_PORT'  => '3306',
 	'APP_DEBUG'  =>  false,
 	'DB_FIELDS_CACHE'=>false,
+
+	'DB_INS_TYPE'  => 'mysql',
+	'DB_INS_HOST'  => '127.0.0.1',//10.0.3.27
+	'DB_INS_NAME'  => 'ins',
+	'DB_INS_USER'  => 'root',
+	'DB_INS_PWD'  => '',
+	'DB_INS_PREFIX'  => '',
+	'DB_INS_CHARSET' => 'utf8',
+	'DB_INS_PORT'  => '3306',
 );
--- Apps/Home/Controller/CronVirusController.class.php
+++ Apps/Home/Controller/CronVirusController.class.php
@@ -25,38 +25,125 @@ class CronVirusController extends CommonController {
         $this->init();
 
         //从mains数据库获取要扫毒的文件
+        $this->log("从mains数据库获取新上传的文件",  'info');
         $this->save_list();
 
         //进行未签名的扫毒
+        $this->log("进行未签名文件的扫毒",  'info');
         $list = $this->get_list(STATUS_INIT);
         $this->scan_virus($list);
 
         //签名,然后扫毒
+        $this->log("签名,然后扫毒",  'info');
         $list = $this->get_list(STATUS_PROGRAM_NO_VIRUS);
         $this->scan_sign($list);
         $this->scan_virus($list);
 
         //签名后有毒，需要更换签名再次扫描的
+        $this->log("签名后有毒的文件，需要更换签名再次扫描的",  'info');
         $list = $this->get_list(STATUS_PROGRAM_STILL_VIRUS);
         $this->scan_sign($list);
         $this->scan_virus($list);
 
         //签名之后没有问题，上传CDN
+        $this->log("签名之后没有问题的文件，上传CDN",  'info');
         $list =  $this->get_list(STATUS_SIGN_NO_VIRUS);
         $this->up_cdn($list);
 
-        $this->end();
+        $this->log("脚本结束运行",  'info');
+    }
+
+    private function init(){
+        $this->log("脚本开始运行",  'info');
+        set_time_limit(0);
+        header("Content-type: text/html; charset=utf-8");
+        if(!IS_CLI){
+            //echo "请在命令行模式下运行此脚本";
+            $this->log("请在命令行模式下运行此脚本",  'info');
+            //exit;
+        }
+
+        //初始化配置文件
+        if(empty($this->config['min_sign_email'])){
+            $this->config['min_sign_email'] = 3;
+        }
+        $this->sign_email_body = str_replace('{n}', $this->config['min_sign_email'], $this->sign_email_body);
+    }
+
+    private function save_list(){
+        $connection = sprintf("mysql://%s:%s@%s:%s/%s", C('DB_INS_USER'), C('DB_INS_PWD'), C('DB_INS_HOST'), C('DB_INS_PORT'), C('DB_INS_NAME'));
+        $this->log(sprintf("DB_INS_HOST=%s,DB_INS_NAME=%s", C('DB_INS_HOST'), C('DB_INS_NAME')),  'info');
+
+        //状态值，1 正常，0删除 2=处理中 3=无毒 4=程序有毒 5=签名有毒
+        $file_list = M('mains', NULL, $connection)->where('status=1')->field('id,path')->select();
+        M('mains', NULL, $connection)->where('status=1')->data(array(
+            "status" => 2,
+        ))->save();
+        $this->log("从mains数据表获取到的新上传文件:" . json_encode($file_list),  'info');
+
+        if(!empty($file_list))    foreach($file_list as $v){
+            M('list_new')->data(array(
+                'mains_id' => $v['id'],
+                'file_path' => $v['path'],
+                'status' => 0,
+                'scan_time' => time(),
+                'email_status' => 0,
+            ))->add();
+        }
+        return $file_list;
+    }
+
+    private function get_list($status){
+        $list = M('list_new')->where('status='.$status)->select();
+        $this->log("从list_new表查询到status={$status}的数据为:" . json_encode($list),  'info');
+        return $list;
+    }
+
+    private function scan_virus($list){
+        $post_url = self::POST_VIRUS_URL . '/index.php?m=Upload&a=Upload';
+        $this->log("扫毒接口的url为:" . $post_url,  'info');
+
+        $post_arr = array();
+        foreach($list as $v){
+            if(class_exists('\CURLFile')){
+                $post_data = array(
+                    "file_path" =>	new \CURLFile($v['file_path']),
+                );
+            }else{
+                $post_data = array(
+                    "file_path" =>	'@' . $v['file_path'],
+                );
+            }
+            $post_data['email_list'] = 'JSON_API';
+            $post_arr[] = $post_data;
+
+            $sign = array_pop(explode('', $v['sign_used']));
+            $data = array(
+                'list_id' => $v['id'],
+                'file_md5' => md5_file($v['file_path']),
+                'status' => 0,
+                'begin_time' => time(),
+                'sign' => $sign == NULL ? '' : $sign,
+            );
+            M('detail_new')->data($data)->add();
+            $this->log("记录到detail_new表中的信息为:" . json_encode($data),  'info');
+        }
+        $this->post($post_url, $post_arr);
     }
 
     private function scan_sign($list){
         //C:\Users\Administrator\Desktop\tool\signtool.exe sign /f C:\Users\Administrator\Desktop\tool\lizhuo1008.pfx /fd sha256 /p worktogether C:\Users\Administrator\Desktop\mssign32.dll
+        $this->log("签名池中的最小签名个数(min_sign_email):" . $this->config['min_sign_email'],  'info');
         $id_list = array();
         $email_id_str = '';
         if(!empty($list))    foreach($list as $k => $v){
+            $this->log("当前处理的数据，在list表中的信息为:" . json_encode($v),  'info');
             //随机得到签名算法
             $v['sign_method'] = array_rand($this->sign_method);
+            $this->log("随机选到的签名算法为:" . $v['sign_method'],  'info');
 
             //获取未使用的签名
+            $this->log("之前已使用过的签名，在签名池中的ID为:" . $v['sign_used'],  'info');
             if(empty($v['sign_used'])){
                 $sign_list = M('sign_pool')->where('status=0')->select();
             }else{
@@ -72,6 +159,7 @@ class CronVirusController extends CommonController {
             $v['sign_pwd'] = $sign_list[0]['sign_pwd'];
             $sign_cmd = sprintf("%s sign /f %s /fd %s /p %s %s", self::BASE_SIGN_URL, $v['sign_path'], $v['sign_method'], $v['sign_pwd'], $v['file_path']);
             system($sign_cmd, $ret);
+            $this->log(sprintf("签名执行的命令为%s,返回值为%s",$sign_cmd, $ret),  'info');
             if($ret !== FALSE){
                 $id_list[] .= $v['id'] . ',';
                 //记录使用过的签名
@@ -98,63 +186,24 @@ class CronVirusController extends CommonController {
         }
 
         if(!empty($email_list)){
+            $this->log(sprintf("发邮件，email:%s,内容为:", $this->email_list, $this->sign_email_body),  'info');
             send_email("签名池通知", $this->sign_email_body, $this->email_list);
         }
         $email_id_str = trim($email_id_str, ',');
-        M('list_new')->where('id IN ('.$email_id_str . ' )')->data(array(
-            'email_status' => 1,
-        ))->save();
-    }
-
-    private function get_list($status){
-        return M('list_new')->where('status='.$status)->select();
-    }
-
-    private function save_list(){
-        $file_list = M('ins.mains')->where('status=1')->select();
-        if(!empty($file_list))    foreach($file_list as $v){
-            M('list_new')->data(array(
-                'mains_id' => $v['id'],
-                'file_path' => $v['path'],
-                'status' => 0,
-                'scan_time' => time(),
-                'email_status' => 0,
+        if(!empty($email_id_str)){
+            M('list_new')->where('id IN ('.$email_id_str . ' )')->data(array(
+                'email_status' => 1,
             ))->save();
         }
-        return $file_list;
     }
 
-    private function scan_virus($list){
-        $post_url = self::POST_VIRUS_URL . '/index.php?m=Upload&a=Upload';
+    private function up_cdn($list){
 
-        $post_arr = array();
-        foreach($list as $v){
-            if(class_exists('\CURLFile')){
-                $post_data = array(
-                    "file_path" =>	new \CURLFile($v['file_path']),
-                );
-            }else{
-                $post_data = array(
-                    "file_path" =>	'@' . $v['file_path'],
-                );
-            }
-            $post_data['email_list'] = 'JSON_API';
-            $post_arr[] = $post_data;
-
-            $sign = array_pop(explode('', $v['sign_used']));
-            M('detail_new')->data(array(
-                'list_id' => $v['id'],
-                'file_md5' => md5_file($v['file_path']),
-                'status' => 0,
-                'begin_time' => time(),
-                'sign' => $sign == NULL ? '' : $sign,
-            ))->add();
-        }
-       $this->post($post_url, $post_arr);
     }
 
     private function post($post_url, $post_arr){
         if(empty($post_arr)){
+            $this->log(sprintf("url为%s,CURL POST数据为空", $post_url),  'info');
             return false;
         }
 
@@ -171,6 +220,7 @@ class CronVirusController extends CommonController {
             curl_setopt($conn[$i], CURLOPT_POST, 1);
             curl_setopt($conn[$i], CURLOPT_POSTFIELDS, $post_data);
             curl_multi_add_handle($mh, $conn[$i]);
+            $this->log(sprintf("url为%s,CURL POST数据为%s:", $post_url, json_encode($post_data)),  'info');
         }
 
         do{
@@ -183,31 +233,6 @@ class CronVirusController extends CommonController {
         }
 
         return $res;
-    }
-
-    private function init(){
-        $this->log("脚本开始运行",  'info');
-        set_time_limit(0);
-        header("Content-type: text/html; charset=utf-8");
-        if(!IS_CLI){
-            //echo "请在命令行模式下运行此脚本";
-            $this->log("请在命令行模式下运行此脚本",  'info');
-            //exit;
-        }
-
-        //初始化配置文件
-        if(empty($this->config['min_sign_email'])){
-            $this->config['min_sign_email'] = 3;
-        }
-        $this->sign_email_body = str_replace('{n}', $this->config['min_sign_email'], $this->sign_email_body);
-    }
-
-    private function up_cdn($list){
-
-    }
-
-    private function end(){
-      $this->log("脚本结束运行",  'info');
     }
 
     private function log($log, $level = 'info'){
