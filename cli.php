eed6d1e7a3f294c664153d705a8a232cb9399f89
吕力 <2302216679@qq.com>
2016-05-18 15:23:19 +0800

    修改细节

--- cli.php
+++ cli.php
@@ -5,16 +5,4 @@ define ('APP_DEBUG', true);
 define('APP_PATH', './Apps/');
 define('MODE_NAME', 'cli');
 
-$depr = DIRECTORY_SEPARATOR;
-$path   = isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : '';
-$params = array();
-if(!empty($path)) {
-	$params = explode($depr, trim($path, $depr));
-}
-!empty($params) ? $_GET['g'] = array_shift($params) : NULL;
-!empty($params) ? $_GET['m'] = array_shift($params) : NULL;
-!empty($params) ? $_GET['a'] = array_shift($params) : NULL;
-if(count($params) > 1) {
-	preg_replace('@(\w+),([^,\/]+)@e', '$_GET[\'\\1\']="\\2";', implode(',',$params));
-}
 require ROOT_PATH . '/ThinkPHP/ThinkPHP.php';
--- index.php
+++ index.php
@@ -3,4 +3,4 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 define('ROOT_PATH', dirname(__FILE__) . '/');
 define('APP_DEBUG',True);
 define('APP_PATH','./Apps/');
-require './ThinkPHP/ThinkPHP.php';
+require ROOT_PATH .'/ThinkPHP/ThinkPHP.php';
