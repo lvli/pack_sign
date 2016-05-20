62bf476b9c52ba32ce02708fc0a19c70a647f70c
吕力 <2302216679@qq.com>
2016-05-20 19:16:35 +0800

    修改url

--- Apps/Home/Conf/config.php
+++ Apps/Home/Conf/config.php
@@ -21,7 +21,7 @@ return array(
 	'DB_INS_PORT'  => '3306',
 
 	'PUT_CDN_DIR' => 'lvli', //main
-	'GGG_DOMAIN_URL' => 'http://ggg.cekaojme.com/',
+	'GGG_DOMAIN_URL' => 'http://ggg.cekaojme.com',
 	'CDN_DOWANLOAD_URL' => 'd3j30ujq5cgnz5.cloudfront.net',
 
 	'DEFAULT_MIN_SIGN_NUM' => 3,//签名池默认最小签名数量
