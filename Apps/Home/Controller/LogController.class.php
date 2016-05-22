9ac4d6645c40d77c2630c452297edb728a56e96a
624707976@qq.com <060810117lvli>
2016-05-22 23:13:44 +0800

    日志页面增加筛选功能

--- Apps/Home/Controller/LogController.class.php
+++ Apps/Home/Controller/LogController.class.php
@@ -4,7 +4,12 @@ use Think\Controller;
 
 class LogController extends CommonController {
 	public function index(){
-		$result = D('Log')->findAll(30);
+		$type = I('request.type', '', 'string');
+		$level = I('request.level', '', 'string');
+		$begin_time = I('request.begin_time', '', 'string');
+		$end_time = I('request.end_time', '', 'string');
+
+		$result = D('Log')->findAll(30, $type, $level, $begin_time, $end_time);
 		$this->assign('list', $result['list']);
 		$this->assign('pagination', $result['pagination']);
 		$this->display();
--- Apps/Home/Model/LogModel.class.php
+++ Apps/Home/Model/LogModel.class.php
@@ -11,12 +11,28 @@ class LogModel extends Model{
 		$this->table = M($this->tableName);
 	}
 
-	function findAll($pageCount){
+	function findAll($pageCount, $type, $level, $begin_time, $end_time){
+		$begin_time = strtotime($begin_time);
+		$end_time = strtotime($end_time);
+		$where = '1=1 ';
+		if(!empty($type)){
+			$where .= " AND  type = '{$type}'";
+		}
+		if(!empty($level)){
+			$where .= " AND  level = '{$level}'";
+		}
+		if(!empty($begin_time)){
+			$where .= " AND  addtime >= '{$begin_time}'";
+		}
+		if(!empty($end_time)){
+			$where .= " AND  addtime <= '{$end_time}'";
+		}
+
 		import('ORG.Util.Page');
 		$count = $this->table->count();
 		if(!empty($count)){
 			$page = new \Org\Util\Page($count, $pageCount);
-			$list = $this->table->order('id DESC')->limit($page->firstRow . ',' . $page->listRows)->select();
+			$list = $this->table->where($where)->order('id DESC')->limit($page->firstRow . ',' . $page->listRows)->select();
 		}
 		if(!empty($list)){
 			foreach($list as &$v){
