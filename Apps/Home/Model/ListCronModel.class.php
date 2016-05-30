18018b1c2feebc3b4a4cc79e0809b2580480ec4c
吕力 <2302216679@qq.com>
2016-05-30 18:53:26 +0800

    修改定时任务的状态

--- Apps/Home/Model/ListCronModel.class.php
+++ Apps/Home/Model/ListCronModel.class.php
@@ -101,12 +101,8 @@ class ListCronModel extends CommonModel{
 	private function get_status_name($status){
 		$status_arr = array(
 			STATUS_INIT => '尚未开始',
-			STATUS_PROGRAM_NO_VIRUS => '程序无毒',
-			STATUS_SIGN_NO_VIRUS => '签名无毒',
-			STATUS_PROGRAM_VIRUS => '程序有毒',
-			STATUS_SIGN_VIRUS => '签名有毒',
-			STATUS_SIGN_STILL_VIRUS_NO_CHECK => '签名后依然有毒,需要用微软程序验证签名是否有毒',
-			STATUS_SIGN_STILL_VIRUS_CHECKED => '确认签名有毒,需要更换签名再次扫描的',
+			STATUS_PROGRAM_NO_VIRUS => 'CDN文件无毒',
+			STATUS_PROGRAM_VIRUS => 'CDN文件有毒',
 		);
 
 		return isset($status_arr[$status]) ? $status_arr[$status] : '';
