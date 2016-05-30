8cdae65e5754e4674c85d1595f9ca7363d701efe
吕力 <2302216679@qq.com>
2016-05-30 19:08:12 +0800

    完善细节

--- Apps/Home/Controller/ListCronController.class.php
+++ Apps/Home/Controller/ListCronController.class.php
@@ -6,13 +6,18 @@ class ListCronController extends CommonController {
 	public function index(){
 		$search_name = I('get.search_name', '', 'string');
 		$scan_times = I('get.scan_times', 0, 'int');
+
 		$result = D('ListCron')->findAll(30, $search_name, $scan_times);
-		$scan_times = D('ListCron')->getScanTimes();
+		$scan_times_list = D('ListCron')->getScanTimes();
+		if(!isset($_GET['scan_times'])) {
+			$scan_times = intval($scan_times_list[0]);
+		}
 
 		$this->assign('list', $result['list']);
 		$this->assign('pagination', $result['pagination']);
 		$this->assign('search_name', $search_name);
 		$this->assign('scan_times', $scan_times);
+		$this->assign('scan_times_list', $scan_times_list);
 		$this->display();
 	}
 
